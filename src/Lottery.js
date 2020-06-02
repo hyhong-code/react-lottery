d84461e4279b09cb53d4ae5c1815893db03eb418
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-01 21:12:42 -0700

    add generate random function

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
 import Ball from "./Ball";
+import "./Lottery.css";
 
 class Lottery extends Component {
+  static defaultProps = {
+    title: "Lotto",
+    maxBalls: 6,
+    maxNum: 40,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // create empty slots for initial load
+      nums: Array.from({ length: this.props.maxBalls }),
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  generate() {
+    const newNums = this.state.nums.map((num) => {
+      return Math.floor(Math.random() * this.props.maxNum + 1);
+    });
+    this.setState({ nums: newNums });
+  }
+
+  handleClick() {
+    this.generate();
+  }
+
   render() {
     return (
       <div>
-        <h1>Title</h1>
+        <h1>{this.props.title}</h1>
         <div>
-          <Ball number={3} />
-          <Ball number={2} />
-          <Ball number={4} />
-          <Ball number={5} />
+          {this.state.nums.map((num) => (
+            <Ball number={num} />
+          ))}
         </div>
-        <button>Generate</button>
+        <button onClick={this.handleClick}>Generate</button>
       </div>
     );
   }
